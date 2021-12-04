c10c139b3a25076c62aa481498568bd537ffbde9
RTTV <tristanandlucky@gmail.com>
2021-12-03 20:45:10 -0500

    Added a Disable Portal Travel toggle (hopefully) and changed Nether Portal Volume to a @ModifyArg

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinNetherPortalBlock.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinNetherPortalBlock.java
@@ -3,6 +3,7 @@ package fi.dy.masa.tweakeroo.mixin;
 import fi.dy.masa.tweakeroo.config.Configs;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.entity.Entity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
@@ -10,19 +11,24 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
 @Mixin(NetherPortalBlock.class)
 public class MixinNetherPortalBlock {
-    @Inject(method = "randomDisplayTick", at = @At("HEAD"), cancellable = true)
-    private void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random, CallbackInfo ci)
+    @ModifyArg(method = "randomDisplayTick(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FFZ)V"), index = 5)
+    private float mixin(float in) {
+        return (float) Configs.Generic.NETHER_PORTAL_VOLUME.getDoubleValue();
+    }
+
+    @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
+    private void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci)
     {
-        if (random.nextInt(100) == 0)
+        if (Configs.Disable.DISABLE_PORTAL_TRAVEL.getBooleanValue())
         {
-            world.playSound((double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, SoundEvents.BLOCK_PORTAL_AMBIENT, SoundCategory.BLOCKS, (float) Configs.Generic.NETHER_PORTAL_VOLUME.getDoubleValue(), random.nextFloat() * 0.4F + 0.8F, false);
+            ci.cancel();
         }
-        ci.cancel();
     }
 }
